70cb536774abe77b2cbb2e69a940c335a59087b5
Sander Spruijt <sander@clockassist.com>
2022-05-06 10:25:56 +0200

    Configured facade

--- src/Prometheus/Laravel/Facade.php
+++ src/Prometheus/Laravel/Facade.php
@@ -2,10 +2,15 @@
 
 namespace Prometheus\Laravel;
 
+use Prometheus\CollectorRegistry;
+
+/**
+ * @see CollectorRegistry
+ */
 class Facade extends \Illuminate\Support\Facades\Facade
 {
     protected static function getFacadeAccessor()
     {
-        //return HubInterface::class;
+        return 'prometheus';
     }
 }
