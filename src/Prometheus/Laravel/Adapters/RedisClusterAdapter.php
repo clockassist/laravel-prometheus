8699403e8797364845e5cc9460d4fc4ffa95b1b1
Sander Spruijt <sander@clockassist.com>
2022-05-06 12:05:47 +0200

    Removed default constructor connection

--- src/Prometheus/Laravel/Adapters/RedisClusterAdapter.php
+++ src/Prometheus/Laravel/Adapters/RedisClusterAdapter.php
@@ -24,7 +24,6 @@ class RedisClusterAdapter implements Adapter
 
     public function __construct()
     {
-        $this->redis = new RedisCluster(null, []);
     }
 
     public static function fromExistingConnection(RedisCluster $redis): self
